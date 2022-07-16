aedc54f431196106c5ba4f0d6055eda0d6ccbf5b
zhushuyin <568412156@qq.com>
2022-07-16 13:17:49 +0800

    12123

--- NetCoreTest/Program.cs
+++ NetCoreTest/Program.cs
@@ -14,7 +14,7 @@ namespace NetCoreTest
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            Console.WriteLine("Test 110");
+            Console.WriteLine("Test 1108908908908");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
