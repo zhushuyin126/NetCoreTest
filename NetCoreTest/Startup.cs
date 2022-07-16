d0423ac3c5a329d05ac516de5bd04295c1dfa47b
zhushuyin <zhushuyin@126.com>
2022-07-16 14:17:27 +0800

    add

--- NetCoreTest/Startup.cs
+++ NetCoreTest/Startup.cs
@@ -24,7 +24,9 @@ namespace NetCoreTest
         {
             Configuration = configuration;
         }
-
+        /// <summary>
+        /// 12
+        /// </summary>
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
